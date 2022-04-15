cacc0bed1dc22cf63aa53e0829bbff6fa8287eb4
AngelicaRoy <ariadna.espiroy@gmail.com>
2022-04-15 16:58:32 -0500

    update mission 1

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,11 @@
+// node main.js
+//import file logger.js
+const logger = require('./logger')
+
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
+
+/**Muestra en pantalla:
+    info: This is an informational message
+    verbose: This is a verbose message
+*/
